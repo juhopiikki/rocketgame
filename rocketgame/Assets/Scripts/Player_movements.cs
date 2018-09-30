7bd3e80f5d8d5dc5a4365b189716fe62d4f80fdb
Juho Piikki <juho.piikki@relex.fi>
2018-09-30 17:55:32 +0300

    Simple gameplay with arrow movements. Not colliding with walls yet.

--- rocketgame/Assets/Scripts/Player_movements.cs
+++ rocketgame/Assets/Scripts/Player_movements.cs
@@ -16,18 +16,28 @@ public class Player_movements : MonoBehaviour {
 
 	void Update()
     {
+//    	Debug.Log("angular: " + rb.angularVelocity);
+//    	Debug.Log("rotation: " + rb.rotation);
+//    	Debug.Log("forward: " + rb.transform.forward);
+    	Debug.Log("right: " + rb.transform.right);
         if (Input.GetKey(KeyCode.UpArrow))
         {
-        	rb.velocity = rb.velocity + new Vector2(0.0f, 1.0f);
+        	rb.velocity = rb.velocity + new Vector2(-rb.transform.right[1] * 0.1f, rb.transform.right[0] * 0.1f);
+        	//rb.AddForce(rb.transform.right * 1);
+//        	rb.velocity = rb.velocity + new Vector2(rb.trasform.rotation, 0.2f);
+//        	rb.angularVelocity = rb.angularVelocity + 0.2f; 
+//        	rb.velocity = rb.velocity + new Vector2(0.0f, 0.2f);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-        	rb.velocity = rb.velocity + new Vector2(-1.0f, 0.0f);
+        	rb.angularVelocity = rb.angularVelocity + 0.6f;
+        	//rb.velocity = rb.velocity + new Vector2(-0.2f, 0.0f);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-        	rb.velocity = rb.velocity + new Vector2(1.0f, 0.0f);
+        	rb.angularVelocity = rb.angularVelocity - 0.6f;
+        	// rb.velocity = rb.velocity + new Vector2(0.2f, 0.0f);
         }
     }
 }
