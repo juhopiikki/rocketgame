4716331d346775eb1d188735684ef5fdbd15f680
Juho Piikki <juho.piikki@relex.fi>
2018-09-30 16:15:07 +0300

    Testing out some simple movements

---
+++ rocketgame/Assets/Scripts/Player_movements.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movements : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	void Awake()
+    {
+        // SpriteRenderer sprRend = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+
+        rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+	void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+        	rb.velocity = rb.velocity + new Vector2(0.0f, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+        	rb.velocity = rb.velocity + new Vector2(-1.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+        	rb.velocity = rb.velocity + new Vector2(1.0f, 0.0f);
+        }
+    }
+}
